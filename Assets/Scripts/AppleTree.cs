2b66ef52202119c96a23efaa2b57911bd5dbe679
Defeng Kong <vergildfkong@hotmail.com>
2019-02-07 20:36:23 -0500

    ApplePicker

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    //Prefab of instantiating apples
+    public GameObject applePrefab; 
+
+    //Speed at which the AppleTree Moves
+    public float speed = 1f;
+
+    //Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    //chance that the appletree will change directions
+    public float chanceToChangeDirection = 0.02f;
+
+    //rate at which apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Dropping apples every second
+        Invoke("DropApple", 2f);
+
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        //Changing Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); //move right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); //move left
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        //changing direction randomly is now time-based because of FixedUpdate
+        if (Random.value < chanceToChangeDirection)
+        {
+            speed *= -1; //change direction
+        }
+    }
+}
---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Basket : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");               
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the current screen position of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;                             
+                                                                             
+        mousePos2D.z = -Camera.main.transform.position.z;                    
+                                                                            
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);    
+                                                                            
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+
+    }
+    void OnCollisionEnter(Collision coll)
+    {                        
+        // Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;                   
+        if (collidedWith.tag == "Apple")
+        {                        
+            Destroy(collidedWith);
+        }
+
+        // Parse the text of the scoreGT into an int
+        int score = int.Parse(scoreGT.text);                          
+                                                                     
+        score += 100;
+        // Convert the score back to a string and display it
+        scoreGT.text = score.ToString();
+
+        if (score > HighScore.score)
+        {
+            HighScore.score = score;
+        }
+
+    }
+
+}
